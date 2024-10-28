9c83424803bb3806969daf34cfa13c9f50c17742
vandhanab <112164783+vandhanab@users.noreply.github.com>
2024-10-28 14:42:45 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,20 @@
+//question-01
+let str = "Hello World!";
+let newStr = str.replace("World!", "My Dear Friend");
+console.log(newStr);  
+//question-02
+let a = "Hungry";
+let repeated = a.repeat(10);
+console.log(repeated);  
+//question-03
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let lastIndex = sentence.lastIndexOf("fox");
+console.log(lastIndex); 
+//question-04
+let message = "Hello, world!";
+let startsWithHi = message.startsWith("hi");
+console.log(startsWithHi); 
+//question-04
+let fileName = "document.pdf";
+let endsWithPdf = fileName.endsWith(".pdf");
+console.log(endsWithPdf);
